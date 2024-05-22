983d4d4d37c02c82e315f9a33194e3017ade998c
bujhm <Bujhm99@gmail.com>
2024-05-22 13:03:34 +0300

    Add tag CRUD

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path
 
-from todo.views import TaskListView, TagsListView
+from todo.views import (
+    TaskListView,
+    TagsListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
+)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
-    path("tags", TagsListView.as_view(), name="tag-list"),
+
+
+    path("tags", TagsListView.as_view(), name="tags-list"),
+    path("tags/create/",
+         TagCreateView.as_view(),
+         name="tag-create"),
+    path("tags/<int:pk>/update/",
+         TagUpdateView.as_view(),
+         name="tag-update"),
+    path("tags/<int:pk>/delete/",
+         TagDeleteView.as_view(),
+         name="tag-delete"),
 
 ]
 app_name = "to_do"
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Tag, Task
@@ -12,5 +13,25 @@ class TaskListView(generic.ListView):
 
 class TagsListView(generic.ListView):
     model = Tag
-    paginate_by = 12
+    template_name = "todo/tags_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("to_do:tags-list")
+    template_name = "todo/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("to_do:tags-list")
+    template_name = "todo/tag_form.html"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("to_do:tags-list")
+    template_name = "todo/tag_confirm_delete.html"
 
